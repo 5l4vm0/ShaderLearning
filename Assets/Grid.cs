afdde8fd2d97966a1fe5c2414ff5d9c3b7bdcc73
hsuanchao1 <123881809+5l4vm0@users.noreply.github.com>
2024-09-16 22:13:16 +0100

    Draw triangles on mesh Filter

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -29,21 +29,35 @@ public class Grid : MonoBehaviour
             for(int x =0; x <= xSize; x++, i++)
             {
                 vertices[i] = new Vector3(x,y);
-                yield return new WaitForSecondsRealtime(0.1f);
             }
         }
 
         //Assign the vertices in vertices array as mesh vertices
         mesh.vertices = vertices;
 
-        int[] triangle = new int[6];
-        triangle[0] = 0;
-        triangle[1] = xSize + 1;//first vertex in next row
-        triangle[2] = 1;
-        triangle[3] = 1;
-        triangle[4] = xSize + 1;
-        triangle[5] = xSize + 2;
-        mesh.triangles = triangle;
+        int[] triangle = new int[xSize * 6 * ySize]; //xsize *6 cause there are xsize*6 vertices for 1 square
+        
+        for(int t = 0, v = 0, y = 0; y < ySize; y++, v++) // v is the index to the veritces array elements
+        {
+            for (int x = 0; x < xSize; x++, t += 6, v++) //t+=6 cause 1 square(2 triangels) used 6 vertice
+            {
+                triangle[t] = v;
+                triangle[t + 1] = triangle[t + 4] = v + xSize + 1;//first vertex in next row
+                triangle[t + 2] = triangle[t + 3] = v + 1;
+                triangle[t + 5] = v + xSize + 2;
+                yield return new WaitForSecondsRealtime(0.1f);
+                mesh.triangles = triangle; //The mesh.triangles array is a list of triangles that contains indices into the vertex array.
+
+            }
+        }
+
+
+        //2 triangles
+        //triangle[0] = 0;
+        //triangle[1] = triangle[4] = xSize + 1;//first vertex in next row
+        //triangle[2] = triangle[3] = 1;
+        //triangle[5] = xSize + 2;
+        
     }
 
     private void OnDrawGizmos()
