ef4ace66654865cc7374c25f37b232368f4ccf6b
hsuanchao1 <123881809+5l4vm0@users.noreply.github.com>
2024-09-18 00:05:10 +0100

    TransformationGrid

---
+++ Assets/TransformationGrid/TransformationGrid.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(MeshRenderer), typeof(MeshFilter))]
+public class TransformationGrid : MonoBehaviour
+{
+    public Transform prefab;
+    public int gridResolution = 10;
+    Transform[] grid;
+    
+    void Awake()
+    {
+        grid = new Transform[gridResolution * gridResolution * gridResolution];
+        
+        for(int i=0, x =0; x < gridResolution; x++)
+        {
+            for(int y = 0; y < gridResolution; y++)
+            {
+                for(int z =0; z< gridResolution; z++, i++)
+                {
+                    grid[i] = CreateGridPoint(x, y, z);
+                }
+            }
+        }
+    }
+
+    private Transform CreateGridPoint(int x,int y,int z)
+    {
+        Transform cube = Instantiate(prefab);
+        cube.localPosition = GetCoordinates(x, y, z);
+        cube.GetComponent<MeshRenderer>().material.color = new Color((float)x / gridResolution, (float)y / gridResolution, (float)z / gridResolution);
+        return cube;
+    }
+    
+    Vector3 GetCoordinates(int x, int y, int z) // Make the centre of the grid to 0,0,0
+    {
+        return new Vector3(x - (gridResolution - 1) / 2, y - (gridResolution - 1) / 2,z - (gridResolution - 1) / 2);
+    }
+}
