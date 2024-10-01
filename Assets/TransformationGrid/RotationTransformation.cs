00fbf7a61f84af07528f99a7e0bda08f83aeb62b
hsuanchao1 <123881809+5l4vm0@users.noreply.github.com>
2024-10-01 22:40:57 +0100

    Rotation matrix

--- Assets/TransformationGrid/RotationTransformation.cs
+++ Assets/TransformationGrid/RotationTransformation.cs
@@ -8,13 +8,44 @@ public class RotationTransformation : Transformation
     public override Vector3 Apply(Vector3 point)
     {
         float radZ = rotation.z * Mathf.Deg2Rad; //converting z rotation degree to radians as sin and cos function in unity expect angles in radians 
+        float radY = rotation.y * Mathf.Deg2Rad;
+        float radX = rotation.x * Mathf.Deg2Rad;
+
+        float sinX = Mathf.Sin(radX);
+        float cosX = Mathf.Cos(radX);
+
+        float sinY = Mathf.Sin(radY);
+        float cosY = Mathf.Cos(radY);
+        
         float sinZ = Mathf.Sin(radZ);
         float cosZ = Mathf.Cos(radZ);
-        return new Vector3 (
-            //x(1,0) + y(0,1) = x(cosZ, sinZ) + y(-sinZ, cosZ) = x*cosZ-y*sinZ, x*sinZ+y*cosZ
-            point.x*cosZ - point.y*sinZ,
-            point.x*sinZ + point.y*cosZ,
-            point.z
+
+        Vector3 xAxis = new Vector3(
+            cosY * cosZ,
+            cosX *sinZ +sinX*sinY*cosZ,
+            sinX * sinZ -cosX*sinY*cosZ
         );
+
+        Vector3 yAxis = new Vector3(
+            -cosY * sinZ,
+            cosX * cosZ - sinX * sinY * sinZ,
+            sinX * cosZ + cosX * sinY * sinZ
+        );
+
+        Vector3 zAxis = new Vector3(
+            sinY,
+            -sinX*cosY,
+            cosX*cosY
+        );
+
+        //Return rotation applied to Z Axis
+        //return new Vector3 (
+        //    //x(1,0) + y(0,1) = x(cosZ, sinZ) + y(-sinZ, cosZ) = x*cosZ-y*sinZ, x*sinZ+y*cosZ
+        //    point.x*cosZ - point.y*sinZ,
+        //    point.x*sinZ + point.y*cosZ,
+        //    point.z
+        //);
+
+        return xAxis * point.x + yAxis * point.y + zAxis * point.z;
     }
 }
